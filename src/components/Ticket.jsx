f5b4f1cf224afb3b399de64994c76a9138d05441
Adilet Momunaliev and Joseph Tubridy <adilet@>
2020-02-10 10:50:40 -0800

    add propTypes

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket(){
+function Ticket(props){
   return (
       <div>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase entries not saving!</em></p>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
         <hr/>
       </div>
    );
 }
+// propTypes is declaring a propTypes property on our Ticket componenent
+// PropTypes is referring to the PropTypes class we import at the top
+Ticket.propTypes = {
+  names: PropTypes.string,
+  locaton: PropTypes.string,
+  issue: PropTypes.string
+};
 
 export default Ticket;
+
+
+/*
+/MyExampleComponent.propTypes = {
+  exampleArray: PropTypes.array,
+  exampleBoolean: PropTypes.bool,
+  exampleFunction: PropTypes.func,
+  exampleNumber: PropTypes.number,
+  exampleObject: PropTypes.object,
+  exampleString: PropTypes.string,
+  exampleSymbol: PropTypes.symbol,
+  exampleReactElement: PropTypes.element,
+
+  exampleArrayOfNumbers: PropTypes.arrayOf(PropTypes.number),
+  exampleArrayOfStrings: PropTypes.arrayOf(PropTypes.string),
+
+  exampleClassTypeProp: PropTypes.instanceOf(ExampleClassName),
+
+  optionalEnum: PropTypes.oneOf(['ExampleClass', 'AnotherExampleClass']),
+}
+*/
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,7 +3,10 @@ import Ticket from "./Ticket";
 
 function TicketList(){
   return (
-    <Ticket/>
+    <Ticket
+      location="3A"
+      names="Potato and Paisley"
+      issue="Firebase will not save record!"/>
   );
 }
 
