f60bc492559f6233c1e973bd9ccd2cbddc464ff6
Adilet Momunaliev and Joseph Tubridy <adilet@>
2020-02-10 13:06:38 -0800

    add info on CSS styling with React

---
+++ src/components/MyStyledComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function MyStyledComponent(props) {
+  var myStyledComponentStyles = {
+    backgoundColor: '#ecg0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
+  return (
+    <div style={myStyledComponentStyles}>
+      <h1>Hey, I'm a component</h1>
+      <h2>But there's something different about me .....</h2>
+      <h3>Unlike other components you have worked with thus far....</h3>
+      <h>I also include custom CSS sytles!</h4>
+      <p>Pretty cool, right</p>
+    </div>
+  );
+}
+
+export default MyStyledComponent;
