1b3c75745d5e7e8611b4976e1b1797c0ecf29101
Jake Ash and Joseph Tubridy <jake@>
2020-02-13 10:08:22 -0800

    Add pictures and the error 404 screen if the page does not exist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
+import Error404 from './Error404';
+import Picture from './Picture';
 
 function App(){
   return (
@@ -11,7 +13,9 @@ function App(){
       <Switch>
         <Route exact path='/' component={TicketList} /> 
         <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
       </Switch>
+      <Picture /> 
     </div>
   );
 }
---
+++ src/components/Picture.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import reallyAdorableKitty from '../assets/images/kitty.jpg';
+
+function Picture(){
+  return (
+    <div>
+      <img src={reallyAdorableKitty}/>
+    </div>
+  );
+}
+
+export default Picture;
