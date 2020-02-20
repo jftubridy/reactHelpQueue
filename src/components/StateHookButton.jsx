464735be129f52d054f2155cefd2d5c2b3063c7a
Joseph Tubridy <jftubridy@gmail.com>
2020-02-20 06:46:10 -0800

    add an example page for how to use hooks to manage state, example button

---
+++ src/components/StateHookButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SomeComponent() {
+
+  const [counter, setCounter] = useState(0);
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <div>
+      {hidden ? <h1>{counter}</h1> : <h1>Count Hidden</h1>}
+      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+    </div>
+  );
+}
